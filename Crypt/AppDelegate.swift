2a13d512fe697a9eacbce46f167fc2247d8bafb1
Dylan Rothfeld <drothfel@gmail.com>
2018-12-15 16:25:14 -0800

    inital commit

---
+++ Crypt/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Crypt
+//
+//  Created by Dylan Rothfeld on 12/15/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
